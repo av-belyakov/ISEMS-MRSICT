86bda593c792af9eaf077d1eec2f856bc5c30296
Artemiy <beljakovav@gmail.com>
2021-02-05 17:43:09 +0300

    ISEMS-MRSICT 05.02.2021

--- datamodels/dataModelBasedOnFromatSTIX.go
+++ datamodels/dataModelBasedOnFromatSTIX.go
@@ -6,8 +6,8 @@ import (
 
 /**********			 Некоторые примитивные типы STIX			 **********/
 
-//ExternalReferencesTypeSTIX тип со списком о внешних ссылках не относящихся к STIX информации
-type ExternalReferencesTypeSTIX []*ExternalReferenceTypeElementSTIX //`json:"external-reference" bson:"external-reference"`
+//ExternalReferencesTypeSTIX тип "external-reference", по терминалогии STIX является списком с информацией о внешних ссылках не относящихся к STIX информации
+type ExternalReferencesTypeSTIX []*ExternalReferenceTypeElementSTIX
 
 //ExternalReferenceTypeElementSTIX тип содержащий подробную информацию о внешних ссылках, таких как URL, ID и т.д.
 // SourceName - имя источника (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
@@ -23,29 +23,26 @@ type ExternalReferenceTypeElementSTIX struct {
 	ExternalID  string         `json:"external_id" bson:"external_id"`
 }
 
-//HashesTypeSTIX тип содержащий хеш хначения, где <тип_хеша>:<хеш>
+//HashesTypeSTIX тип "hashes", по терминалогии STIX, содержащий хеш хначения, где <тип_хеша>:<хеш>
 type HashesTypeSTIX map[string]string
 
-//IdentifierTypeSTIX тип содержащий уникальный идентификатор UUID, преимущественно версии 4
-// Type - тип идентификатора, берется из пространства имен спецификации STIX или формируется самостоятельно, пример: <ipv4_addr> или <external-source> (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
-// ID - уникальный идентификатор в формате UUID, формируется по принципу <external-source--ff26c055-6336-5bc5-b98d-13d6226742dd> (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
-// Value - может содержать какую либо дополнительную информацию
-type IdentifierTypeSTIX struct {
-	Type  string `json:"type" bson:"type"`
-	ID    string `json:"id" bson:"id"`
-	Value string `json:"value" bson:"value"`
-}
+//IdentifierTypeSTIX тип "identifier", по терминалогии STIX, содержащий уникальный идентификатор UUID, преимущественно версии 4 при этом ID должен
+//начинаться с наименования организации или программного обеспечения сгенерировавшего его. Например, <example-source--ff26c055-6336-5bc5-b98d-13d6226742dd> (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+type IdentifierTypeSTIX string
 
-//KillChainPhasesTypeSTIX тип содержащий цепочки фактов, приведшей к какому либо урону
+//KillChainPhasesTypeSTIX тип "kill-chain-phases", по терминалогии STIX, содержащий цепочки фактов, приведших к какому либо урону
 type KillChainPhasesTypeSTIX []*KillChainPhasesTypeElementSTIX //`json:"kill_chain_phases" bson:"kill_chain_phases"`
 
-//KillChainPhasesTypeElementSTIX тип содержащий набор элементов цепочки фактов, приведшей к какому либо урону
+//KillChainPhasesTypeElementSTIX тип содержащий набор элементов цепочки фактов, приведших к какому либо урону
 // KillChainName - имя цепочки (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
 // PhaseName - наименование фазы из спецификации STIX, например, "reconnaissance", "pre-attack" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
 type KillChainPhasesTypeElementSTIX struct {
 	KillChainName string `json:"kill_chain_name" bson:"kill_chain_name"`
 	PhaseName     string `json:"phase_name" bson:"phase_name"`
 }
+
+//OpenVocabTypeSTIX тип "open-vocab", по терминалогии STIX, содержащий заранее определенное (предложенное) значение
+type OpenVocabTypeSTIX string
 
 /********** 			Data Markings STIX 			**********/
 
@@ -60,17 +57,17 @@ type CommonDataMarkingsTypeSTIX struct {
 	Created     time.Time `json:"created" bson:"created"`
 }
 
-//GranularMarkingsTypeSTIX тип определяет как тип MarkingDefinitionObjectSTIX ссылается на свойства "marking_ref" и "lang"
-// MarkingRef
-// Lang
-// Selectors
+//GranularMarkingsTypeSTIX тип "granular_markings", по терминалогии STIX, представляет собой набор маркеров ссылающихся на свойства "marking_ref" и "lang"
+// Lang - идентифицирует язык соответствующим маркером
+// MarkingRef - определяет идентификатор объекта "marking-definition"
+// Selectors - определяет список селекторов для содержимого объекта STIX, к которому применяется это свойство
 type GranularMarkingsTypeSTIX struct {
-	MarkingRef `json:"marking_ref" bson:"marking_ref"`
-	Lang `json:"lang" bson:"lang"`
-	Selectors `json:"selectors" bson:"selectors"`
+	Lang       string             `json:"lang" bson:"lang"`
+	MarkingRef IdentifierTypeSTIX `json:"marking_ref" bson:"marking_ref"`
+	Selectors  []string           `json:"selectors" bson:"selectors"`
 }
 
-//MarkingDefinitionObjectSTIX объект определения маркировки содержит метки данных ссылающиеся на требования к обработке или совместному использованию данных
+//MarkingDefinitionObjectSTIX объект "marking-definition", по терминалогии STIX, содержит метки данных ссылающиеся на требования к обработке или совместному использованию данных
 // Type - наименование типа объекта, для этого типа это поле ДОЛЖНО содержать "marking-definition" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
 // Name - наименование маркера
 // DefinitionType - определение типа, должно содержать "statement" или "tlp"
@@ -95,7 +92,7 @@ type MarkingDefinitionObjectSTIX struct {
 
 /********** 			Domain Objects STIX 			**********/
 
-//CommonPropertiesObjectSTIX общие, для всех объектов STIX, свойства
+//CommonPropertiesObjectSTIX свойства общие, для всех объектов STIX, свойства
 // Type - наименование типа шаблона, для этого типа это поле ДОЛЖНО содержать "attack-pattern" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
 // SpecVersion - версия спецификации STIX используемая для представления текущего объекта (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
 // ID - уникальный идентификатор объекта (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
@@ -107,9 +104,10 @@ type MarkingDefinitionObjectSTIX struct {
 //  в диапазоне 0-100. Если 0 - значение не определено.
 // Lang - содержит текстовый код языка, на котором написан контент объекта. Для английского это "en" для русского "ru"
 // ExternalReferences - список внешних ссылок не относящихся к STIX информации
-// ObjectMarkingRefs - определяет список свойств идентификаторов объектов определения маркировки, которые применяются к этому объекту
-//  хотя оно и является списком типа IdentifierTypeSTIX, но тот в свою очередь ССЫЛАЕТСЯ на объект типа MarkingDefinitionObjectSTIX (marking-definition)
+// ObjectMarkingRefs - определяет список ID ссылающиеся на объект "marking-definition", по терминалогии STIX, в котором содержатся значения применяющиеся к этому объекту
 // GranularMarkings - определяет список "гранулярных меток" (granular_markings) относящихся к этому объекту
+// Defanged - определяет были ли определены данные содержащиеся в объекте
+// Extensions - может содержать дополнительную информацию, относящуюся к объекту
 type CommonPropertiesObjectSTIX struct {
 	Type               string                     `json:"type" bson:"type"`
 	SpecVersion        string                     `json:"spec_version" bson:"spec_version"`
@@ -123,14 +121,80 @@ type CommonPropertiesObjectSTIX struct {
 	ExternalReferences ExternalReferencesTypeSTIX `json:"external_references" bson:"external_references"`
 	ObjectMarkingRefs  []*IdentifierTypeSTIX      `json:"object_marking_refs" bson:"object_marking_refs"`
 	GranularMarkings   GranularMarkingsTypeSTIX   `json:"granular_markings" bson:"granular_markings"`
+	Defanged           bool                       `json:"defanged" bson:"defanged"`
+	Extensions         map[string]string          `json:"extensions" bson:"extensions"`
 }
 
-//AttackPatternDomainObjectsSTIX объект "шаблон атаки" описывающий способы компрометации цели
-// Name - имя используемое для идентификации шаблона атаки (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+//AttackPatternDomainObjectsSTIX объект "Attack Pattern", по терминалогии STIX, описывающий способы компрометации цели
+// Name - имя используемое для идентификации "Attack Pattern" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+// Description - более подробное описание атаки
+// Aliases - альтернативные имена
+// KillChainPhases - список цепочки фактов, приведших к урону
 type AttackPatternDomainObjectsSTIX struct {
 	CommonPropertiesObjectSTIX
+	Name            string                            `json:"name" bson:"name"`
+	Description     string                            `json:"description" bson:"description"`
+	Aliases         []string                          `json:"aliases" bson:"aliases"`
+	KillChainPhases []*KillChainPhasesTypeElementSTIX `json:"kill_chain_phases" bson:"kill_chain_phases"`
+}
 
-	Name string `json:"name" bson:"name"`
+//CampaignDomainObjectsSTIX объект "Campaign", по терминалогии STIX, набор действий определяющих злонамеренную деятельность или атаки
+// Name - имя используемое для идентификации "Campaign" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+// Description - более подробное описание
+// Aliases - альтернативные имена используемые для идентификации "Campaign"
+// FirstSeen - время когда компания была впервые обнаружена
+// LastSeen - время когда компания была зафиксирована в последний раз
+// Objective - основная цель, желаемый результат или предполагаемый эффект
+type CampaignDomainObjectsSTIX struct {
+	CommonPropertiesObjectSTIX
+	Name        string    `json:"name" bson:"name"`
+	Description string    `json:"description" bson:"description"`
+	Aliases     []string  `json:"aliases" bson:"aliases"`
+	FirstSeen   time.Time `json:"first_seen" bson:"first_seen"`
+	LastSeen    time.Time `json:"last_seen" bson:"last_seen"`
+	Objective   string    `json:"objective" bson:"objective"`
+}
+
+//CourseOfActionDomainObjectsSTIX объект "Course of Action", по терминалогии STIX, описывающий совокупность действий направленных
+// на предотвращение (защиту) либо реагирование на текущую атаку
+// Name - имя используемое для идентификации "Course of Action" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+// Description - более подробное описание
+// Action - ЗАРЕЗЕРВИРОВАНО
+type CourseOfActionDomainObjectsSTIX struct {
+	CommonPropertiesObjectSTIX
+	Name        string      `json:"name" bson:"name"`
+	Description string      `json:"description" bson:"description"`
+	Action      interface{} `json:"action" bson:"action"`
+}
+
+//GroupingDomainObjectsSTIX объект "Grouping", по терминалогии STIX, объединяет различные объекты STIX в рамках какого то общего контекста
+// Name - имя используемое для идентификации "Course of Action" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+// Description - более подробное описание
+// Context - заранее определенное (предложенное) значение (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+// ObjectRefs - указывает объекты STIX, на которые ссылается эта группировка (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+type GroupingDomainObjectsSTIX struct {
+	CommonPropertiesObjectSTIX
+	Name        string                `json:"name" bson:"name"`
+	Description string                `json:"description" bson:"description"`
+	Context     OpenVocabTypeSTIX     `json:"context" bson:"context"`
+	ObjectRefs  []*IdentifierTypeSTIX `json:"object_refs" bson:"object_refs"`
+}
+
+//IdentityDomainObjectsSTIX объект "Identity", по терминалогии STIX, содержит основную идентификационную информацию физичиских лиц, организаций и т.д.
+// Name - имя используемое для идентификации "Identity" (ОБЯЗАТЕЛЬНОЕ ЗНАЧЕНИЕ)
+// Description - более подробное описание
+// Roles - список ролей для идентификации действий
+// IdentityClass - заранее определенное (предложенное) значение физического лица или организации
+// Sectors - заранее определенный (предложенный) перечень отраслей промышленности, к которой принадлежит физическое лицо или организация
+// ContactInformation -
+type IdentityDomainObjectsSTIX struct {
+	CommonPropertiesObjectSTIX
+	Name               string               `json:"name" bson:"name"`
+	Description        string               `json:"description" bson:"description"`
+	Roles              []string             `json:"roles" bson:"roles"`
+	IdentityClass      OpenVocabTypeSTIX    `json:"identity_class" bson:"identity_class"`
+	Sectors            []*OpenVocabTypeSTIX `json:"sectors" bson:"sectors"`
+	ContactInformation string               `json:"contact_information" bson:"contact_information"`
 }
 
 //`json:"" bson:""`

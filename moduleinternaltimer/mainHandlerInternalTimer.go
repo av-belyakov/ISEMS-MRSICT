c0eb1214637848a4c8521601f01ccf5694f672d3
Artemij Belyakov <beljakovav@gmail.com>
2021-03-10 13:09:07 +0300

    ISEMS-MRSICT 10.03.2021

---
+++ moduleinternaltimer/mainHandlerInternalTimer.go
@@ -0,0 +1,9 @@
+package moduleinternaltimer
+
+import (
+	"fmt"
+)
+
+func mainHandlerInternalTimer() {
+	fmt.Println("func 'mainHandlerInternalTimer', START...")
+}
---
+++ modulereferencebook/mainHandlerReferenceBook.go
@@ -0,0 +1,9 @@
+package modulereferencebook
+
+import (
+	"fmt"
+)
+
+func mainHandlerReferenceBook() {
+	fmt.Println("func 'mainHandlerReferenceBook', START...")
+}

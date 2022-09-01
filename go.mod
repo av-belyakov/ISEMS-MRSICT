3e09434a1f4c0af5312da75e606ad687710d2a24
Artemy Belyakov <beljakovav@gmail.com>
2022-09-01 13:38:45 +0300

    ISEMS-MRSICT 01.09.2022

--- go.mod
+++ go.mod
@@ -9,10 +9,8 @@ require (
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/websocket v1.4.2
 	github.com/onsi/ginkgo v1.16.4
-	github.com/onsi/gomega v1.13.0
+	github.com/onsi/ginkgo/v2 v2.1.6 // indirect
+	github.com/onsi/gomega v1.20.1
 	github.com/signalsciences/ipv4 v1.4.0
 	go.mongodb.org/mongo-driver v1.5.0
-	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
-	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
-	golang.org/x/tools v0.1.2 // indirect
 )
---
+++ mytest/regexp_hex_test.go
@@ -0,0 +1,50 @@
+package mytest_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	//"ISEMS-MRSICT/mytest"
+
+	"regexp"
+)
+
+//HashesTypeSTIX тип "hashes", по терминологии STIX, содержащий хеш значения, где <тип_хеша>:<хеш>
+type HashesTypeSTIX map[string]string
+
+//CheckHashesTypeSTIX выполняет проверку значений типа HashesTypeSTIX
+func (htstix *HashesTypeSTIX) CheckHashesTypeSTIX() bool {
+	if len(*htstix) == 0 {
+		return true
+	}
+
+	pattern := regexp.MustCompile(`^[0-9a-zA-Z-_=]+$`)
+	for k, v := range *htstix {
+		if !pattern.MatchString(k) {
+			return false
+		}
+
+		if !pattern.MatchString(v) {
+			return false
+		}
+	}
+
+	return true
+}
+
+var _ = Describe("RegexpHex", func() {
+	Context("Тест 1. Выполняем проверку регулярного выражения", func() {
+		It("При валидном содержимом должно быть TRUE", func() {
+			htstix := HashesTypeSTIX{
+				"MD5":      "dwffw33gg3",
+				"SHA-1":    "gg4g4g4tg4hg4gh4",
+				"SHA3-256": "g4344g4g3efg3f3f3",
+				//"SHA3-256": "jtjtj6",
+				//"SHA-1":    "ngnmty66j6j",
+				//"MD5":      "6h66hrghtjhymymthnngn",
+			}
+
+			Expect(htstix.CheckHashesTypeSTIX()).Should(BeTrue())
+		})
+	})
+})
